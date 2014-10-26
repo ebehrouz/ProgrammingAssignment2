26e3cf26b54147b1189b86ce4d91c03867937e2a
ebehrouz <brcm68@gmail.com>
2014-10-26 13:58:28 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function makes an R object that initializes a variable ##'m', which will be used to save inverse matrix. It Provides ##function get() to obtain "raw" matrix 
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x=matrix()) {
+    m <- NULL
+    get <- function() x
+    setImatrix <- function(Imatrix) m <<- Imatrix
+    getImatrix <- function() m
 
-makeCacheMatrix <- function(x = matrix()) {
-
+    # This function returns the list of functions as an R object.
+    list(get=get, setImatrix=setImatrix, getImatrix=getImatrix)
+}
+cacheSolve <- function(x) {
+    m <- x$getImatrix()
+    if(!is.null(m)){
+        message("Results")
+        return(m)
+    }
+    else {
+        message("Inverse matrix")
+        data <- x$get() 
+        m <- solve(data) 
+        x$setImatrix(m) # assigns inverse matrix to object x
+        message("Done.")
+        return(m)
+    }
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
